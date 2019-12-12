bc42c18bbbf8b2aeff13e045fa515a7906c58d07
ugsgame <670563380@qq.com>
2019-12-12 09:45:22 +0800

    Add Setting

---
+++ Plugins/UScriptEditor/Source/ScriptEditor/Private/ScriptEditorSetting.cpp
@@ -0,0 +1,14 @@
+﻿
+#include "ScriptEditorSetting.h"
+
+UScriptEdtiorSetting* UScriptEdtiorSetting::Get()
+{
+	static UScriptEdtiorSetting* Singleton = nullptr;
+	if (Singleton == nullptr)
+	{
+		Singleton = NewObject<UScriptEdtiorSetting>();
+		Singleton->AddToRoot();
+	}
+	return Singleton;
+}
+
---
+++ Plugins/UScriptEditor/Source/ScriptEditor/Private/ScriptEditorSetting.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ScriptEditorSetting.generated.h"
+
+
+UCLASS(config = Editor)
+class  UScriptEdtiorSetting : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	static UScriptEdtiorSetting* Get();
+
+	UPROPERTY(config)
+	TArray<FString> EdittingFiles;
+
+	//For UCodeProjectItem scanning files optimization
+	TArray<class UCodeProjectItem*> PreEdittingItems;
+};
